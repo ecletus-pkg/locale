3e7ffe27f0cf24aa8a319ea35cdb4a912d70c1ff
Moises P. Sena <moisespsena@gmail.com>
2018-10-01 12:09:26 -0300

    Add Languages attribute

--- locale.go
+++ locale.go
@@ -16,12 +16,25 @@ func DefaultLocale() string {
 var DEFAULT_LOCALE = DefaultLocale()
 
 type Locale struct {
-	Default string
+	Default   string
+	Languages []string
 }
 
 func (l *Locale) SetDefaults() *Locale {
 	if l.Default == "" {
 		l.Default = DEFAULT_LOCALE
+	}
+	var hasDefault bool
+
+	for _, lang := range l.Languages {
+		if lang == l.Default {
+			hasDefault = true
+			break
+		}
+	}
+
+	if !hasDefault {
+		l.Languages = append(l.Languages, l.Default)
 	}
 	return l
 }
